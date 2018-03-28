eec0ab67247a2b27a07b671f708d7c3f55afe632
Brittany Morris <brittmmendez@gmail.com>
2018-03-28 13:17:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function displayRepositories() {
 
 function getCommits(el) {
 let username=document.getElementById("username").value
-  const name = el.dataset.repo
+  const repoName = el.dataset.repository
   const req = new XMLHttpRequest()
   req.addEventListener("load", displayCommits)
   req.open("GET", 'https://api.github.com/repos/' + username + '/' + name + '/commits')
