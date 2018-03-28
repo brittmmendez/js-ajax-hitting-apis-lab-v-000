5eb95ab37919e27caede360b87f49121b3a14683
Brittany Morris <brittmmendez@gmail.com>
2018-03-28 13:06:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function getRepositories() {
   let username=document.getElementById("username").value
   const req = new XMLHttpRequest()
-  req.addEventListener("load", showRepositories);
+  req.addEventListener("load", displayRepositories);
   req.open("GET", 'https://api.github.com/users/' + username + '/repos')
   req.send()
 }
 
-function showRepositories(event, data) {
+function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
